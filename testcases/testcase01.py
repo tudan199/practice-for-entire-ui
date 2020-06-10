7ac981231c671c9c0b44a2b4196f4a66186cfdd8
tudan199 <819626116@qq.com>
2020-06-10 10:12:59 +0800

    practice for entire ui

---
+++ testcases/testcase01.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import sys
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+print(sys.path)
+sys.path.append('C:/Users/eagle/Desktop/practise/practice for entire ui/driver')
+driver=webdriver.Chrome(executable_path="./driver/chromedriver.exe")
+driver.get('http://www.baidu.com')
+driver.find_element_by_id('kw').send_keys('great!')
+
+WebDriverWait(driver,10).until(lambda  s: s.find_element('id','su')).click()
+driver.maximize_window()
+time.sleep(5)
+driver.quit()
