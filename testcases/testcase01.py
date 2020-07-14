c7fc5a0325f8327098acbcf0603cbc7086000850
tudan199 <819626116@qq.com>
2020-07-14 16:26:09 +0800

    use unittest frame

--- testcases/testcase01.py
+++ testcases/testcase01.py
@@ -2,14 +2,16 @@ from selenium import webdriver
 import sys
 import time
 from selenium.webdriver.support.ui import WebDriverWait
+import unittest
 print(sys.path)
-# if this can push to git
-sys.path.append('C:/Users/eagle/Desktop/practise/practice for entire ui/driver')
-driver=webdriver.Chrome(executable_path="./driver/chromedriver.exe")
-driver.get('http://www.baidu.com')
-driver.find_element_by_id('kw').send_keys('great!')
+class Myunittest(unittest.TestCase):
+    def test_01(self):
+        sys.path.append('C:/Users/eagle/Desktop/practise/practice for entire ui/driver')
+        driver=webdriver.Chrome(executable_path="./driver/chromedriver.exe")
+        driver.get('http://www.baidu.com')
+        driver.find_element_by_id('kw').send_keys('great!')
 
-WebDriverWait(driver,10).until(lambda  s: s.find_element('id','su')).click()
-driver.maximize_window()
-time.sleep(5)
-driver.quit()
+        WebDriverWait(driver,10).until(lambda  s: s.find_element('id','su')).click()
+        driver.maximize_window()
+        time.sleep(5)
+        driver.quit()
