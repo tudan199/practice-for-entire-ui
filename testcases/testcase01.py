47a98c369682ea63a4f937130af71366e50ec960
tudan199 <819626116@qq.com>
2020-07-14 15:38:59 +0800

    another

--- testcases/testcase01.py
+++ testcases/testcase01.py
@@ -3,6 +3,7 @@ import sys
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 print(sys.path)
+# if this can push to git
 sys.path.append('C:/Users/eagle/Desktop/practise/practice for entire ui/driver')
 driver=webdriver.Chrome(executable_path="./driver/chromedriver.exe")
 driver.get('http://www.baidu.com')
